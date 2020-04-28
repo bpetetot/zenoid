593722e9ffd59b7477ec0f53908c2e111f96157e
Benjamin Petetot <bpetetot@gmail.com>
2020-04-28 13:56:21 +0200

    :lipstick: set board size

--- app.jsx
+++ app.jsx
@@ -4,20 +4,25 @@ import {createBlessedRenderer} from 'react-blessed';
 
 const render = createBlessedRenderer(blessed);
 
+const config = {
+	width: 50,
+	height: 25,
+};
+
 class App extends Component {
   render() {
     return (
-      <box label="react-blessed demo" border={{type: 'line'}} style={{border: {fg: 'cyan'}}}>
-        ⚽️
+      <box left="center" top="center" width={80} height={15} border={{type: 'line'}} style={{border: {fg: 'cyan'}}}>
+        0
       </box>
     );
   }
 }
 
 const screen = blessed.screen({
-  autoPadding: true,
-  smartCSR: true,
-  title: 'Zenoid'
+  title: 'Zenoid',
+  smartCSR: false,
+  debug: true,
 });
 
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
