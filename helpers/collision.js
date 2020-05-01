807a76030ee152aa0b3a255ae8919c797a2675e8
Charles-Henri GUÉRIN <charleshenri.guerin@icloud.com>
2020-05-01 22:16:05 +0200

    :recycle: Refactor collision

--- helpers/collision.js
+++ helpers/collision.js
@@ -20,12 +20,10 @@ export const fromRight = (ball, brick) => (
   ball.x === brick.x + brick.width
 )
 
-export const willCollide = box1 => box2 => {
-  return (
-    box1.x < box2.x + box2.width &&
-    box1.x + box1.width > box2.x &&
-    box1.y < box2.y + box2.height &&
-    box1.y + box1.height > box2.y
-  )
-}
+export const willCollide = box1 => box2 => (
+  box1.x < box2.x + box2.width &&
+  box1.x + box1.width > box2.x &&
+  box1.y < box2.y + box2.height &&
+  box1.y + box1.height > box2.y
+)
 
